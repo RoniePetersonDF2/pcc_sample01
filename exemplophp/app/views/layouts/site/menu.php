7838fb9fef9e95127bb3f80028ca9a98e93e1ca5
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-05-01 22:06:04 -0300

    Add projeto inicial

---
+++ exemplophp/app/views/layouts/site/menu.php
@@ -0,0 +1,17 @@
+<header class="main_header">
+    <div class="main_header_content">
+        <a href="#" class="logo">
+            <img src="assets/img/logo.png" alt="Bem vindo ao projeto prático Html5 e Css3 Essentials" title="Bem vindo ao projeto prático Html5 e Css3 Essentials"></a>
+
+        <nav class="main_header_content_menu">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="#escola">Escola</a></li>
+                <li><a href="#contato">Contato</a></li>
+                <li><a href="" class="modal-link">Login</a>
+
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ exemplophp/app/views/site/login.php
@@ -0,0 +1,17 @@
+<!--POP LOGIN-->
+<div class="overlay"></div>
+<div class="modal">
+
+    <div class="div_login">
+        <form action="opcao.html" method="get">
+            <h1>Login</h1><br>
+            <input type="text" placeholder="Nome" class="input">
+            <br><br>
+            <input type="password" placeholder="Senha" class="input">
+            <br><br>
+            <button class="button">Enviar</button>
+        </form>
+    </div>
+
+</div>
+<!--FIM POP LOGIN-->
