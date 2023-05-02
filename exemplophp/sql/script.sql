419cd513db6e8efe1c205b36117431e6e5062b2b
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-05-02 17:46:52 -0300

    Add script DML de usuarios

--- exemplophp/sql/script.sql
+++ exemplophp/sql/script.sql
@@ -1,3 +1,4 @@
+-- SCRIPT DE DDL - DATA DEFINITION LANGUAGE
 -- apaga o banco de dados escolabd, se ele existir.
 DROP SCHEMA IF EXISTS pccsampledb;
 
@@ -62,3 +63,11 @@ CREATE TABLE pccsampledb.comentarios (
     curso_id            INTEGER NOT NULL,
     FOREIGN KEY (curso_id) REFERENCES pccsampledb.cursos(id)
 );
+
+-- SCRIPT DE DML - DATA MANIPULATION LANGUAGE
+-- inserir dados na tabela de usuario. 
+INSERT INTO pccsampledb.usuarios (email, password, nome, perfil, status) 
+    VALUES ('admin@email.com', md5('123'), 'Admin', 'ADM', 1), 
+    ('gerente@email.com', md5('1234'), 'Gerente', 'GER', 0), 
+    ('editor@email.com', md5('12345'), 'Editor', 'EDI', 0), 
+    ('usuario@email.com', md5('123456'), 'Usuário', 'USU', 1); 
