3e95668f94c82bdd1d64d2724676bcd4c3ffa1bb
RoniePetersonDF2 <ronie.peterson.santos@gmail.com>
2023-05-16 21:24:50 -0300

    feature: atualizar usuario

--- exemplophp/app/views/usuario_admin_upd.php
+++ exemplophp/app/views/usuario_admin_upd.php
@@ -14,6 +14,12 @@
         exit;
     }
 
+    # verifica se uma variavel id foi passada via GET 
+    $id = isset($_GET['id']) ? $_GET['id'] : 0;
+    
+    # cria a variavel $dbh que vai receber a conexão com o SGBD e banco de dados.
+    $dbh = Conexao::getInstance();
+
     # verifica se os dados do formulario foram enviados via POST 
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         # cria variaveis (email, nome, perfil, status) para armazenar os dados passados via método POST.
@@ -22,9 +28,6 @@
         $perfil = isset($_POST['perfil']) ? $_POST['perfil'] : 'USU';
         $status = isset($_POST['status']) ? $_POST['status'] : 0;
         
-        # cria a variavel $dbh que vai receber a conexão com o SGBD e banco de dados.
-        $dbh = Conexao::getInstance();
-
         # cria uma consulta banco de dados atualizando um usuario existente. 
         # usando como parametros os campos nome e password.
         $query = "UPDATE `pccsampledb`.`usuarios` SET `EMAIL` = :email,
@@ -44,37 +47,37 @@
         # se sim, redireciona para a pagina de admin com mensagem de sucesso.
         # se não, redireciona para a pagina de cadastro com mensagem de erro.
         if($stmt->rowCount()) {
-            header('location: index.php?success=Cadastro realizado com sucesso! Aguarde o administrador liberar seu acesso');
+            header('location: usuario_admin_list.php?success=Usuário atualizado com sucesso!');
         } else {
-            header('location: usuario_admin_new.php?error=Erro ao cadastrar nova conta!');
+            $error = $dbh->errorInfo();
+            var_dump($error);
+            header('location: usuario_admin_new.php?error=Erro ao atualizar o usuário!');
         }
 
         # destroi a conexao com o banco de dados.
         $dbh = null;
     }
-
-    # verifica se uma variavel id foi passada via GET 
-    $id = isset($_GET['id']) ? $_GET['id'] : 0;
     
     # cria uma consulta banco de dados buscando todos os dados da tabela usuarios 
     # filtrando pelo id do usuário.
     $query = "SELECT * FROM `pccsampledb`.`usuarios` WHERE id=:id LIMIT 1";
     $stmt = $dbh->prepare($query);
     $stmt->bindParam(':id', $id);
-        
+
     # executa a consulta banco de dados e aguarda o resultado.
     $stmt->execute();
     
     # Faz um fetch para trazer os dados existentes, se existirem, em um array na variavel $row.
     # se não existir retorna null
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
     
     # se o resultado retornado for igual a NULL, redireciona para a pagina de listar usuario.
     # se não, cria a variavel row com dados do usuario selecionado.
     if(!$row){
-        header('location: usuario_admin_lis.php?error=Usuário inválido.');
+        header('location: usuario_admin_list.php?error=Usuário inválido.');
     }
-
+    
     # destroi a conexao com o banco de dados.
     $dbh = null;
 ?>
@@ -103,20 +106,26 @@
                     <input type="email" 
                         name="email" 
                         placeholder="Informe seu e-mail."
-                        required autofocus><br><br>
+                        required autofocus
+                        value="<?=isset($row)? $row['email'] : ''?>"
+                        ><br><br>
                     <label for="nome">Nome</label><br>
-                    <input type="text" name="nome" placeholder="Informe seu nome."  required><br><br>
+                    <input type="text" 
+                        name="nome" 
+                        placeholder="Informe seu nome."  
+                        value="<?=isset($row)? $row['nome'] : ''?>"
+                        required><br><br>
                     <label for="perfil">Perfil</label><br>
                     <select name="perfil"><br><br>
-                        <option value="USU">Usuário</option>
-                        <option value="EDI">Editor</option>
-                        <option value="GER">Gerente</option>
-                        <option value="ADM">Administrador</option>
+                        <option value="USU" <?=isset($row) && $row['perfil'] == 'USU'? 'selected' : ''?>>Usuário</option>
+                        <option value="EDI" <?=isset($row) && $row['perfil'] == 'EDI'? 'selected' : ''?>>Editor</option>
+                        <option value="GER" <?=isset($row) && $row['perfil'] == 'GER'? 'selected' : ''?>>Gerente</option>
+                        <option value="ADM" <?=isset($row) && $row['perfil'] == 'ADM'? 'selected' : ''?>>Administrador</option>
                     </select><br><br>
                     <label for="status">Status</label><br>
                     <select name="status"><br><br>
-                        <option value="1">Ativo</option>
-                        <option value="0">Inativo</option>
+                        <option value="1" <?=isset($row) && $row['status'] == '1'? 'selected' : ''?>>Ativo</option>
+                        <option value="0" <?=isset($row) && $row['status'] == '0'? 'selected' : ''?>>Inativo</option>
                     </select>
                     <input type="submit" value="Salvar" name="salvar">
                </form>
